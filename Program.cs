b2a652b9c963bc2228ba24bcbd7a2c8729154834
michalizak <mzakutin@student.touro.edu>
2023-02-19 12:08:43 -0500

    file changed

--- Program.cs
+++ Program.cs
@@ -1,23 +1,30 @@
 ﻿//Michali Zakutinsky
 //T00399092
-
-Console.WriteLine("Hello, World!");
-Console.WriteLine("begin program 3.");
-Console.WriteLine("testing commit.")
-Console.WriteLine("N\tN^2\tN^3\tN^2+N^3");
-Console.WriteLine();
-while (Console.ReadLine() != null)
+namespace Program3
 {
-    for (int i = 10; i <= 10000000; i += 10)
+    class Program
     {
-        for (int x = 1; x <= 3; x++)
+        static void Main(string[] args)
+        {
+            Console.WriteLine("N\tN^2\tN^3\tN^2+N^3");
+            Console.WriteLine();
+            Console.ReadLine() == 'y'
+            while (Console.ReadLine() != null)
             {
-                Console.Write(i ^ x + "\t");
-                if (x == 3)
+                for (int i = 10; i <= 10000000; i += 10)
                 {
-                    Console.Write((i ^ 2) + (i ^ 3));
+                    for (int x = 1; x <= 3; x++)
+                    {
+                        Console.Write(i ^ x + "\t");
+                        if (x == 3)
+                        {
+                            Console.Write((i ^ 2) + (i ^ 3));
+                        }
+                    }
                 }
             }
+
+
+        }
     }
-    
 }
