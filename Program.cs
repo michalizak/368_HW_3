19d36a76251780abf58667d44869a43b8b7113d4
michalizak <mzakutin@student.touro.edu>
2023-02-19 12:54:14 -0500

    works!

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿//Michali Zakutinsky
 //T00399092
+//Michali Zakutinsky
+//T00399092
 namespace Program3
 {
     class Program
@@ -8,23 +10,21 @@ namespace Program3
         {
             Console.WriteLine("N\tN^2\tN^3\tN^2+N^3");
             Console.WriteLine();
-            Console.ReadLine() == 'y'
-            while (Console.ReadLine() != null)
+            for (int i = 10; i <= 50; i += 10)
             {
-                for (int i = 10; i <= 10000000; i += 10)
+                for (int x = 1; x <= 3; x++)
                 {
-                    for (int x = 1; x <= 3; x++)
+                    Console.Write((Math.Pow(i,x)) + "\t");
+                    if (x == 3)
                     {
-                        Console.Write(i ^ x + "\t");
-                        if (x == 3)
-                        {
-                            Console.Write((i ^ 2) + (i ^ 3));
-                        }
+                        Console.WriteLine((Math.Pow(i, 2)) + (Math.Pow(i, 3)));
                     }
                 }
             }
 
 
         }
+
+
     }
 }
